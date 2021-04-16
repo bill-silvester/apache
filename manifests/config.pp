2a84942157055723c198f45a13eb4750e573623e
jazzsaxcat <bill.silvester@d4t4solutions.com>
2021-04-16 15:12:20 +0000

    Added host exported resource

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,15 +4,15 @@
 #
 # @example
 #   include apache::config
-class apache::config (
-  $config_path   = $apache::params::config_path,
-  $config_source = $apache::params::config_source,
-  ) inherits apache::params {
+class apache::config {
   file { 'apache_config':
-    path   => $config_path,
-    source => $config_path,
     ensure => $apache::config_ensure,
+    path   => $apache::config_path,
+    source => $puppet:///modules/apache/${osfamily}.conf",
     notify => Service['apache_service'],
+    mode   => '0664',
+    owner  => 'root',
+    group  => 'root',
   }
   @@host { "${hostname}":
     host_aliases => "$fqdn",
